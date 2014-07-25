aa95e0bb12b13b1a9459c9120c2e6327aedc974c
yoloxigu <yoloxigu@gmail.com>
2014-07-25 12:06:33 +0100

    update

--- app/scripts/controllers/templateCtl.js
+++ app/scripts/controllers/templateCtl.js
@@ -5,6 +5,7 @@ angular.module('haochiquanApp')
             footer: 'views/templates/footer.html',
             ad: 'views/templates/ad.html',
             publish: 'views/templates/publish.html',
+            publish_subject_dialog: 'views/templates/publish_subject_dialog.html',
             content: 'views/templates/content.html',
             userinfo: 'views/templates/userinfo.html',
             usergroup: 'views/templates/usergroup.html'
